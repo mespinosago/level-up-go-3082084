8419a5af02efc7409d7f696cbd1797a0d51ed93c
mespinosago <mespinosago@gmail.com>
2024-02-14 16:55:08 +0000

    Update main.go
    Signed-off-by: mespinosago <mespinosago@gmail.com>

--- 02_05b/main.go
+++ 02_05b/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 )
 
 // setup constants
@@ -16,16 +17,33 @@ type coffeeShop struct {
 
 	orderCoffee  chan struct{}
 	finishCoffee chan struct{}
+
+	mx sync.RWMutex
 }
 
 // registerOrder ensures that the order made by the baristas is counted
 func (p *coffeeShop) registerOrder() {
+	defer p.mx.Unlock()
+	p.mx.Lock()
 	p.orderCount++
+}
+
+func (p *coffeeShop) timeToClose() bool {
+	defer p.mx.RUnlock()
+	p.mx.RLock()
+	if p.orderCount >= maxOrderCount {
+		return true
+	}
+	return false
 }
 
 // barista is the resource producer of the coffee shop
 func (p *coffeeShop) barista(name string) {
 	for {
+		log.Println(p.orderCount)
+		if p.timeToClose() {
+			return
+		}
 		select {
 		case <-p.orderCoffee:
 			p.registerOrder()
@@ -38,6 +56,9 @@ func (p *coffeeShop) barista(name string) {
 // customer is the resource consumer of the coffee shop
 func (p *coffeeShop) customer(name string) {
 	for {
+		if p.timeToClose() {
+			return
+		}
 		select {
 		case p.orderCoffee <- struct{}{}:
 			log.Printf("%s orders a coffee!", name)
@@ -55,11 +76,20 @@ func main() {
 		orderCoffee:  orderCoffee,
 		finishCoffee: finishCoffee,
 	}
+	var wg sync.WaitGroup
+	wg.Add(baristaCount + customerCount)
 	for i := 0; i < baristaCount; i++ {
-		go p.barista(fmt.Sprint("Barista-", i))
+		go func(i int) {
+			defer wg.Done()
+			p.barista(fmt.Sprint("Barista-", i))
+		}(i)
 	}
 	for i := 0; i < customerCount; i++ {
-		go p.customer(fmt.Sprint("Customer-", i))
+		go func(i int) {
+			defer wg.Done()
+			go p.customer(fmt.Sprint("Customer-", i))
+		}(i)
 	}
+	wg.Wait()
 	log.Println("The Level Up Go coffee shop has closed! Bye!")
 }
