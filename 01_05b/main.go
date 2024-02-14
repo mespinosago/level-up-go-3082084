cbee2fbf35d31913529dba75d74a85555951ea10
mespinosago <mespinosago@gmail.com>
2024-02-14 16:50:16 +0000

    Update main.go
    Signed-off-by: mespinosago <mespinosago@gmail.com>

--- 01_05b/main.go
+++ 01_05b/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"sort"
 )
 
 const path = "items.json"
@@ -20,7 +21,24 @@ type SaleItem struct {
 // matchSales adds the sales procentage of the item
 // and sorts the array accordingly.
 func matchSales(budget float64, items []SaleItem) []SaleItem {
-	panic("NOT IMPLEMENTED")
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].ReducedPrice < items[j].ReducedPrice
+	})
+	var inBudgetList []SaleItem
+	spent := 0.0
+	for _, item := range items {
+		if spent+item.ReducedPrice > budget {
+			break
+		}
+		spent += item.ReducedPrice
+		item := item
+		item.SalePercentage = (item.OriginalPrice - item.ReducedPrice) * 100 / item.OriginalPrice
+		inBudgetList = append(inBudgetList, item)
+	}
+	sort.Slice(inBudgetList, func(i, j int) bool {
+		return inBudgetList[i].SalePercentage < inBudgetList[j].SalePercentage
+	})
+	return inBudgetList
 }
 
 func main() {
@@ -39,12 +57,12 @@ func printItems(items []SaleItem) {
 		log.Println("No items found.:( Try increasing your budget.")
 	}
 	for i, r := range items {
-		log.Printf("[%d]:%s is %.2f OFF! Get it now for JUST %.2f!\n", 
-		i, r.Name, r.SalePercentage, r.ReducedPrice)
+		log.Printf("[%d]:%s is %.2f OFF! Get it now for JUST %.2f!\n",
+			i, r.Name, r.SalePercentage, r.ReducedPrice)
 	}
 }
 
-// importData reads the raffle entries from file and 
+// importData reads the raffle entries from file and
 // creates the entries slice.
 func importData() []SaleItem {
 	file, err := os.ReadFile(path)
